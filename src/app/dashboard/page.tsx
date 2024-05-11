bda38d74cf4b25a79957b5f77bf376bc84bf38c1
omar12al1234 <62028638+Omar-Alnajjar-dev@users.noreply.github.com>
2024-05-11 19:02:07 +0300

    dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import { changePremium, changeUsername, getSession } from "@/actions";
 import { redirect } from "next/navigation";
 
-const ProfilePage = async () => {
+const DashboardPage = async () => {
     const session = await getSession();
 
     if (!session.isLoggedIn) {
@@ -9,19 +9,19 @@ const ProfilePage = async () => {
     }
 
     return (
-        <div className="profile">
-            <h1>Dashboard</h1>
+        <div className="login border p-14 bg-white rounded-lg p-b-4">
+            <h1 className="text-3xl">Dashboard</h1>
             <p>
-                Welcome, <b>{session.username}</b>
+                Welcome, <b>{session.username}</b> <br />. What would you like to do today?
             </p>
             <span>You are <b>{session.isAdmin ? "An Admin" : "a Patient"}</b> user</span>
 
-            <form action={changeUsername}>
-                <input type="text" name="username" required placeholder={session.username} />
-                <button>Update</button>
-            </form>
+            <div className="flex flex-col justify-center items-center">
+                <button className="text-blue-600 border border-blue-600 rounded-lg p-3 m-2 hover:text-white hover:bg-blue-600 active:scale-95">Update Patients Records</button>
+                <button className="text-blue-600 border border-blue-600 rounded-lg p-3 m-2 hover:text-white hover:bg-blue-600 active:scale-95">Manage Access Control</button>
+            </div>
         </div>
     );
 };
 
-export default ProfilePage;
+export default DashboardPage;
