a0f5f081db2aa3bab6eecba1204ae18787c2611e
omar12al1234 <62028638+Omar-Alnajjar-dev@users.noreply.github.com>
2024-05-11 19:18:25 +0300

    made the ui of dashboard change based on role

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -12,14 +12,14 @@ const DashboardPage = async () => {
         <div className="login border p-14 bg-white rounded-lg p-b-4">
             <h1 className="text-3xl">Dashboard</h1>
             <p>
-                Welcome, <b>{session.username}</b> <br />. What would you like to do today?
+                Welcome, <b>{session.username}</b>. <br />What would you like to do today?
             </p>
             <span>You are <b>{session.isAdmin ? "An Admin" : "a Patient"}</b> user</span>
 
-            <div className="flex flex-col justify-center items-center">
+            {session.isAdmin && <div className="flex flex-col justify-center items-center m-4">
                 <button className="text-blue-600 border border-blue-600 rounded-lg p-3 m-2 hover:text-white hover:bg-blue-600 active:scale-95">Update Patients Records</button>
                 <button className="text-blue-600 border border-blue-600 rounded-lg p-3 m-2 hover:text-white hover:bg-blue-600 active:scale-95">Manage Access Control</button>
-            </div>
+            </div>}
         </div>
     );
 };
--- src/components/logoutForm.tsx
+++ src/components/logoutForm.tsx
@@ -3,7 +3,7 @@ import { logout } from "@/actions"
 const LogoutForm = () => {
   return (
     <form action={logout}>
-      <button className="bg-red-600 rounded-lg p-2 m-2">Logout</button>
+      <button className="bg-red-600 rounded-lg p-2 m-2 text-white">Logout</button>
     </form>
   )
 }
